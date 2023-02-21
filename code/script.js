00eea61c629b2fe0c772532e8dd82b354ebd7182
Bruno S. T. Hofman <brunohofman2004@gmail.com>
2023-02-21 20:36:20 -0300

    escolha jogador e escolha robô funcionando

--- code/script.js
+++ code/script.js
@@ -1,8 +1,28 @@
-let playerChoice = document.querySelector('div.playerChoice')
-let robotChoice = document.querySelector('div.robotChoice')
+
 let res = document.querySelector('section.res')
 let robotList = ['rock', 'paper', 'scissors']
+let robotRandomChoice = ''
+let imgChoicePlayer = document.getElementById('imgChoicePlayer')
+let imgChoiceRobot = document.getElementById('imgChoiceRobot')
+
 
 function choiceRock(){
-    
+    imgChoicePlayer.src = '../img/rock-50.png'
+    robotControl()
+    //adcionar resultado (ifs)
+}
+
+function choicePaper(){
+    imgChoicePlayer.src = '../img/paper-50.png'
+    robotControl()
+}
+
+function choiceScissors(){
+    imgChoicePlayer.src = '../img/scissors-50.png'
+    robotControl()
+}
+
+function robotControl(){
+    robotRandomChoice = robotList[Math.floor(Math.random() * robotList.length)]
+    imgChoiceRobot.src = `../img/${robotRandomChoice}-50.png`
 }
