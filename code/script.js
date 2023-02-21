7ec51ad6213107f1ca564e83746b318cb0a88ae3
Bruno S. T. Hofman <brunohofman2004@gmail.com>
2023-02-21 15:07:29 -0300

    estrutura básica

---
+++ code/script.js
@@ -0,0 +1,8 @@
+let playerChoice = document.querySelector('div.playerChoice')
+let robotChoice = document.querySelector('div.robotChoice')
+let res = document.querySelector('section.res')
+let robotList = ['rock', 'paper', 'scissors']
+
+function choiceRock(){
+    
+}
